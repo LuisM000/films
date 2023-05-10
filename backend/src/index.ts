5d381c43ce5a335847e39eaa478dba2cb6ba770c
LuisM000 <luis.m.r@outlook.com>
2023-05-10 18:30:25 +0200

    Added front and back projects

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const PORT = 3000
+
+app.get('/', (_requ, res) => {
+  res.send('Hello world!!!')
+})
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+})
