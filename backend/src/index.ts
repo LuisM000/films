5e0c7a2c301a4b7ad209507dc47e31011d23322a
LuisM000 <luis.m.r@outlook.com>
2023-05-11 22:27:49 +0200

    Try fix vercel deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,10 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 
 app.get('/', (_requ, res) => {
-  res.send('Hello world!!!')
+  res.send('Hello world')
 })
 
 app.listen(PORT, () => {
