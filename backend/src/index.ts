bc52093a51069d60b02291b64875bed0e311d8c1
LuisM000 <luis.m.r@outlook.com>
2023-05-11 23:02:14 +0200

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ app.use(express.json())
 const PORT = process.env.PORT || 3000
 
 app.get('/', (_requ, res) => {
-  res.send('Hello world')
+  res.send('Hello world!!')
 })
 
 app.listen(PORT, () => {
