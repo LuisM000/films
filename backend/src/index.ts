35f6b6f0464d20da1690441081db230a9ce3dc57
LuisM000 <luis.m.r@outlook.com>
2023-05-19 22:24:33 +0200

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,19 @@
-import { Server } from './infrastructure/express/server'
+/* import { Server } from './infrastructure/express/server'
 import { Container } from './shared/infrastructure/container'
 
 const container = new Container()
 const server = container.invoke().resolve<Server>('server')
 
-server.start()
+server.start() */
+
+import express from 'express'
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/api/films/all', (_req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
