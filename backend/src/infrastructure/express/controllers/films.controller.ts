0a71f4926fa65020da4a7efc5a054a3631fcbac6
LuisM000 <luis.m.r@outlook.com>
2023-05-19 23:30:42 +0200

    Fix controllers

--- backend/src/infrastructure/express/controllers/films.controller.ts
+++ backend/src/infrastructure/express/controllers/films.controller.ts
@@ -1,14 +1,16 @@
 import { route, GET } from 'awilix-router-core'
+import { FilmService } from '@/application/filmService'
 import { Request, Response } from 'express'
 
 @route('/api/films')
 export class FilmsController {
 
+  constructor(private filmService: FilmService) {
+  }
+
   @route('/all')
   @GET()
   getAllFilms(_request: Request, response: Response) {
-    response
-      .status(200)
-      .send('FIlms')
+    response.status(200).send(this.filmService.getAll())
   }
 }
