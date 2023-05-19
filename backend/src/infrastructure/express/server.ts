74ead7ca195f53c6caffb5e536006fdfb8f3befb
LuisM000 <luis.m.r@outlook.com>
2023-05-19 19:58:55 +0200

    Test change port to vercel deploy

--- backend/src/infrastructure/express/server.ts
+++ backend/src/infrastructure/express/server.ts
@@ -4,11 +4,13 @@ import { App } from './app'
 
 export class Server {
 
+  PORT = process.env.PORT || 3000
+
   constructor(private app: App, private config: Configuration, private logger: ServerLogger) {
   }
 
   public start() {
-    this.app.expressApp.listen(this.config.PORT, () => {
+    this.app.expressApp.listen(this.PORT, () => {
       this.logger.info(`server is running on port ${this.config.PORT}. Environment ${this.config.NODE_ENV}`)
     })
   }
