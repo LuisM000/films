ec7b5a12ddc9f2b0d0f0666b0c70aec7dd43b5ab
LuisM000 <luis.m.r@outlook.com>
2023-05-19 22:53:53 +0200

    Update server.ts

--- backend/src/infrastructure/express/server.ts
+++ backend/src/infrastructure/express/server.ts
@@ -1,6 +1,7 @@
 import { Container } from '@/shared/infrastructure/container'
 import { loadControllers, scopePerRequest } from 'awilix-express'
 import express from 'express'
+//import path from 'path'
 
 export class Server {
 
@@ -13,7 +14,11 @@ export class Server {
     const app = express()
       .use(express.json())
       .use(scopePerRequest(this.container.invoke()))
-      .use(loadControllers('controllers/*.ts', { cwd: __dirname }))
+      .use(loadControllers('src/infrastructure/express/controllers/*.ts'))
+
+    //const configFilePath = path.join(__dirname, 'config', 'app.config.js')
+    //const controllersPath = path.join(path.dirname(configFilePath), 'controllers')
+    //app.use(loadControllers(`${controllersPath}/*.ts`))
 
     app.get('/api/other-films/all', (_req, res) => {
       res.send('Hello World!' + __dirname)
