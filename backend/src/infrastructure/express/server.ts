2787c8547374acb5c98460851022bdf9df9f86a8
LuisM000 <luis.m.r@outlook.com>
2023-05-19 22:58:29 +0200

    Update server.ts

--- backend/src/infrastructure/express/server.ts
+++ backend/src/infrastructure/express/server.ts
@@ -21,11 +21,67 @@ export class Server {
     //app.use(loadControllers(`${controllersPath}/*.ts`))
 
     app.get('/api/other-films/all', (_req, res) => {
-      res.send('Hello World!' + __dirname)
+      const directoryPath = __dirname
+      const tree = getDirectoryTree(directoryPath)
+      res.send('Hello World!' + printDirectoryTree(tree))
     })
 
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`)
     })
+
+    function printDirectoryTree(tree: FileNode, indent = ''): string {
+      let result = `${indent}${tree.name}\n`
+
+      if (tree.children) {
+        tree.children.forEach((child) => {
+          result += printDirectoryTree(child, `${indent}  |`)
+        })
+      }
+
+      return result
+    }
   }
+
+
 }
+
+import fs from 'fs'
+import path from 'path'
+
+interface FileNode {
+  path: string;
+  name: string;
+  children?: FileNode[];
+}
+
+function getDirectoryTree(directoryPath: string): FileNode {
+  const stats = fs.statSync(directoryPath)
+  if (!stats.isDirectory()) {
+    throw new Error(`${directoryPath} is not a directory.`)
+  }
+
+  const directoryTree: FileNode = {
+    path: directoryPath,
+    name: path.basename(directoryPath),
+    children: [],
+  }
+
+  const files = fs.readdirSync(directoryPath)
+  files.forEach((file) => {
+    const filePath = path.join(directoryPath, file)
+    const fileStats = fs.statSync(filePath)
+
+    if (fileStats.isDirectory()) {
+      directoryTree.children?.push(getDirectoryTree(filePath))
+    } else {
+      directoryTree.children?.push({
+        path: filePath,
+        name: file,
+      })
+    }
+  })
+
+  return directoryTree
+}
+
