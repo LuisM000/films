6979f6aaa513373546a3f0986d10589340ecae27
LuisM000 <luis.m.r@outlook.com>
2023-05-19 20:08:17 +0200

    Update server.ts

--- backend/src/infrastructure/express/server.ts
+++ backend/src/infrastructure/express/server.ts
@@ -1,15 +1,14 @@
 import { Configuration } from '../../shared/infrastructure/config'
 import { ServerLogger } from '../logger'
 import { App } from './app'
-import * as http from 'http'
 
 export class Server {
 
   constructor(private app: App, private config: Configuration, private logger: ServerLogger) {
   }
 
-  public start(): http.Server {
-    return this.app.expressApp.listen(this.config.PORT, () => {
+  public start() {
+    this.app.expressApp.listen(this.config.PORT, () => {
       this.logger.info(`server is running on port ${this.config.PORT}. Environment ${this.config.NODE_ENV}`)
     })
   }
