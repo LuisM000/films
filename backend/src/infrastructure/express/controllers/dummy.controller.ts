d7bd32b823bcf4e2b34f32255d4099baa5bda6c1
LuisM000 <luis.m.r@outlook.com>
2023-05-19 22:37:48 +0200

    More test deploy

--- backend/src/infrastructure/express/controllers/dummy.controller.ts
+++ backend/src/infrastructure/express/controllers/dummy.controller.ts
@@ -1,14 +1,14 @@
 import { route, GET } from 'awilix-router-core'
 import { Request, Response } from 'express'
 
-@route('/api/films')
-export class FilmsController {
+@route('/api/dummy')
+export class DummyController {
 
   @route('/all')
   @GET()
   getAllFilms(_request: Request, response: Response) {
     response
       .status(200)
-      .send('FIlms')
+      .send('ook')
   }
 }
--- backend/src/infrastructure/express/server.ts
+++ backend/src/infrastructure/express/server.ts
@@ -1,14 +1,19 @@
-import { loadControllers } from 'awilix-express'
+import { Container } from '@/shared/infrastructure/container'
+import { loadControllers, scopePerRequest } from 'awilix-express'
 import express from 'express'
 
 export class Server {
 
+  constructor(private container: Container) {
+  }
 
   public start() {
     const port = process.env.PORT || 3000
 
     const app = express()
       .use(express.json())
+      .use(scopePerRequest(this.container.invoke()))
+
       .use(loadControllers('controllers/*.ts', { cwd: __dirname }))
 
     app.listen(port, () => {
