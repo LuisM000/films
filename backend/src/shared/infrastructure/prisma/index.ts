793c264984a343ce901f865e600a6b2a1feb31c4
LuisM000 <luis.m.r@outlook.com>
2023-05-23 23:48:48 +0200

    Update index.ts

--- backend/src/shared/infrastructure/prisma/index.ts
+++ backend/src/shared/infrastructure/prisma/index.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from '@prisma/client'
 import { Configuration } from '../config'
 
 export function createPrismaClient(config: Configuration): PrismaClient {
+  console.log({ config })
+  console.log('db ' + config.DATABASE_URL)
   return new PrismaClient({
     datasources: {
       db: {
