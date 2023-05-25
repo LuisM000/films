8e89f77a107eb16bce87e0b2d4f9704fe66a85db
LuisM000 <luis.m.r@outlook.com>
2023-05-25 19:24:27 +0200

    Try fix env variables

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import * as dotenv from 'dotenv'
 import path from 'path'
+const { NODE_ENV } = process.env
+
+dotenv.config()
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+export default defineConfig(() => {
   return {
     plugins: [react()],
     resolve: {
@@ -13,7 +18,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: process.env.API_URL ?? env.API_URL,
+          target: process.env.API_URL,
           changeOrigin: true,
         },
       },
