d0e4aaca14a6de6af2cb31d4904305edcc7ae048
LuisM000 <luis.m.r@outlook.com>
2023-05-25 18:15:52 +0200

    Load api target from env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000/',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 })
