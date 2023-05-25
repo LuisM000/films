39c4121ae26b4bf086eac74138b452b679ba3b7f
LuisM000 <luis.m.r@outlook.com>
2023-05-25 18:50:05 +0200

    Add cors

--- backend/src/infrastructure/express/app.ts
+++ backend/src/infrastructure/express/app.ts
@@ -2,12 +2,14 @@ import express, { Application } from 'express'
 import { loadControllers, scopePerRequest } from 'awilix-express'
 import path from 'path'
 import { Container } from '../../shared/infrastructure/container'
+import cors from 'cors'
 
 export class App {
   public readonly expressApp: Application
 
   constructor(container: Container) {
     this.expressApp = express()
+      .use(cors())
       .use(express.json())
       .use(scopePerRequest(container.invoke()))
       .use(loadControllers(`controllers/*${path.extname(__filename)}`, { cwd: __dirname }))
