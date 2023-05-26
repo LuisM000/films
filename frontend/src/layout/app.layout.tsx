0a97a37ac55bb8ef54dd1e8d12f4f4c114379087
LuisM000 <luis.m.r@outlook.com>
2023-05-26 17:57:35 +0200

    Minor app layout design

--- frontend/src/layout/app.layout.tsx
+++ frontend/src/layout/app.layout.tsx
@@ -2,9 +2,9 @@ import { Outlet } from 'react-router-dom'
 
 export const AppLayout: React.FC = () => {
   return (
-    <div>
+    <div className='flex flex-col h-screen'>
       <h1>Header</h1>
-      <main>
+      <main className='flex-grow'>
         <Outlet />
       </main>
       <h1>Footer</h1>
