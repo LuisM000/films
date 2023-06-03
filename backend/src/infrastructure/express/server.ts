ce42901cf768bc4bb7ac4e4cba1953248e661f19
LuisM000 <luis.m.r@outlook.com>
2023-06-03 11:20:09 +0200

    More CI tests

--- backend/src/infrastructure/express/server.ts
+++ backend/src/infrastructure/express/server.ts
@@ -10,9 +10,6 @@ export class Server {
   public start() {
     this.app.expressApp.listen(this.config.PORT, () => {
       this.logger.info(`server is running on port ${this.config.PORT}. Environment ${this.config.NODE_ENV}`)
-      const db = this.config.DATABASE_URL
-      const lenght = db.length
-      this.logger.info(`server DB ${db} with leng ${lenght}`)
     })
   }
 }
