e0427be428c4fab601b79c1adef4481d8a870c95
LuisM000 <luis.m.r@outlook.com>
2023-06-03 09:43:06 +0200

    Add more cypress test

--- e2e-tests/cypress/cypress/e2e/films.cy.js
+++ e2e-tests/cypress/cypress/e2e/films.cy.js
@@ -6,4 +6,16 @@ describe('Films page', () => {
   it('smoke test: frontpage can be opened', () => {
     cy.contains('Películas mierder')
   })
+
+  it('should contains a film and can be navigate', () => {
+    cy.get('ul')
+      .children('li')
+      .contains('Sweeney Todd: The Demon Barber of Fleet Street')
+      .click()
+
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.match(/^\/film\/\w+$/);
+    })
+  })
+
 })
