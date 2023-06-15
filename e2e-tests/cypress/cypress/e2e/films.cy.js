10c6fbb998c813e6aa4822c33deb9fb438b88fbc
LuisM000 <luis.m.r@outlook.com>
2023-06-15 10:40:21 +0200

    Fix cypress test

--- e2e-tests/cypress/cypress/e2e/films.cy.js
+++ e2e-tests/cypress/cypress/e2e/films.cy.js
@@ -1,3 +1,5 @@
+import '@testing-library/cypress/add-commands'
+
 beforeEach(() => {
   cy.visit('/')
 })
@@ -8,13 +10,13 @@ describe('Films page', () => {
   })
 
   it('should contains a film and can be navigate', () => {
-    cy.get('ul')
-      .children('li')
+    cy.findByRole('list')
+      .findByRole('listitem')
       .contains('Sweeney Todd: The Demon Barber of Fleet Street')
       .click()
 
     cy.location().should((loc) => {
-      expect(loc.pathname).to.match(/^\/film\/\w+$/);
+      expect(loc.pathname).to.match(/^\/films\/\w+$/);
     })
   })
 
