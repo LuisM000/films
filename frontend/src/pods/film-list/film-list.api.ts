6f88f39a37043b405b81c325c40c1f9714917882
LuisM000 <luis.m.r@outlook.com>
2023-06-16 08:29:40 +0200

    Add barrel file

--- frontend/src/pods/film-list/film-list.api.ts
+++ frontend/src/pods/film-list/film-list.api.ts
@@ -1,6 +1,5 @@
-import { apiRoutes } from '@/common-app/api-configuration/api.constants'
+import { apiRoutes, get } from '@/common-app/api-configuration'
 import { BasicFilms } from 'common/models/basicFilm'
-import { get } from '@/common-app/api-configuration/api'
 
 export const getFilms = (): Promise<BasicFilms> => {
   return get<BasicFilms>(apiRoutes.films.list)
