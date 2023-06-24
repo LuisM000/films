4fb826540b8c96a0cae1dc0ba1a01532af13a29f
LuisM000 <luis.m.r@outlook.com>
2023-06-24 18:01:00 +0200

    Change loading color

--- frontend/src/common/components/loading.component.tsx
+++ frontend/src/common/components/loading.component.tsx
@@ -1,7 +1,7 @@
 export const LoadingComponent = () => {
   return (
     <div role="status" aria-hidden="true" className="flex items-start justify-center h-screen mt-20">
-      <div className="w-20 h-20 rounded-full animate-spin border-y-2 border-solid border-green-600 border-t-transparent"></div>
+      <div className="w-20 h-20 rounded-full animate-spin border-y-2 border-solid bg-purple-600 border-t-transparent"></div>
     </div>
   )
 }
