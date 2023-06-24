d148b46e2b08a401ddab47926a2ba0c866e80e75
LuisM000 <luis.m.r@outlook.com>
2023-06-24 16:46:02 +0200

    Add new navbar

--- frontend/src/layout/app.layout.tsx
+++ frontend/src/layout/app.layout.tsx
@@ -1,13 +1,17 @@
+import { NavbarComponent } from '@/common-app/components/navbar.component'
 import { Outlet } from 'react-router-dom'
 
 export const AppLayout: React.FC = () => {
+
   return (
-    <div className='flex flex-col h-screen bg-darker'>
-      <h1>Películas mierder</h1>
-      <main className='flex-grow'>
+    <div className='flex flex-col h-screen'>
+      <header>
+        <NavbarComponent />
+      </header>
+      <main className='flex-grow pt-20'>
         <Outlet />
       </main>
-      <h1>Footer</h1>
+      <footer>Footer</footer>
     </div>
   )
 }
